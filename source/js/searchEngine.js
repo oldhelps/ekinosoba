ce85825b2c72e1dde885421ccabfb8c8a0ca5b78
oldhelps <oldhelps@126.com>
2025-08-27 17:41:44 +0800

    Added search at search engine.

--- source/js/searchEngine.js
+++ source/js/searchEngine.js
@@ -1,24 +1,22 @@
-window.addEventListener('DOMContentLoaded', function() {
-function search(engine) { 
+
+document.querySelector("body > header > div > div.nav-main.container.container--flex > div.m_search > form").addEventListener('submit',engine=>{
     const keyword =document.querySelector("body > header > div > div.nav-main.container.container--flex > div.m_search > form > input").value
-    switch (engine) { 
+    switch (SEARCH_SERVICE) { 
         case "baidu":
-            window.open("https://www.baidu.com/s?wd=" + keyword)
+            window.open("https://www.baidu.com/s?wd=" + keyword+"site:"+window.location.host)
             break;
         case "google":
-            window.open("https://www.google.com/search?q=" + keyword)
+            window.open("https://www.google.com/search?q=" + keyword+"site:"+window.location.host)
             break;
         case "bing":
-            window.open("https://www.bing.com/search?q=" + keyword)
+            window.open("https://www.bing.com/search?q=" + keyword+"site:"+window.location.host)
             break;
         case "yahoo":
-            window.open("https://search.yahoo.com/search?p=" + keyword)
+            window.open("https://search.yahoo.com/search?p=" + keyword+"site:"+window.location.host)
             break;
-        case "so":
-            window.open("https://www.sogou.com/web?query=" + keyword)
+        case "sougou":
+            window.open("https://www.sogou.com/web?query=" + keyword+"site:"+window.location.host)
             break;
 
     }
-    return false;
-}
-}, false);
+})
